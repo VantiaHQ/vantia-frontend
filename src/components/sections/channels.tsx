82cd74aad88f1ef337128583aaf5e3b6af01b736
Coke Stuyck <cokecancook@gmail.com>
2025-09-25 01:28:26 +0200

    Update channels.tsx
    Darker bg

--- src/components/sections/channels.tsx
+++ src/components/sections/channels.tsx
@@ -12,7 +12,7 @@ const channels = [
 
 export default function Channels() {
 	return (
-		<section id="channels" className="bg-secondary py-20 sm:py-24">
+		<section id="channels" className="bg-[#070916] py-20 sm:py-28">
 			<div className="container mx-auto px-6">
 				<div className="mx-auto max-w-2xl text-center">
 					<h2 className="text-3xl font-bold tracking-tight text-blue-200 sm:text-4xl">
